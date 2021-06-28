d9b003e6c7eca9b4ea41828cfd8d6e9876c26555
Ramazan <ramazan.halid.35@gmail.com>
2021-06-28 22:29:03 +0300

    Splash Screen added

--- src/Screens/SplashScreen.js
+++ src/Screens/SplashScreen.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {ActivityIndicator, View, Image} from 'react-native';
 
-const SplashScreen = () => {
-  return <View>
-      
-  </View>;
+import styles from '../styles/styles';
+const SplashScreen = ({navigation}) => {
+  const [animating, setAnimating] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimating(false);
+      navigation.replace('Authentication');
+    }, 4500);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/Images/bInfoGISLOGO.png')}
+        style={{width: '90%', resizeMode: 'contain', margin: 30}}
+      />
+      <ActivityIndicator
+        animating={animating}
+        color="#006400"
+        size="large"
+        style={styles.activityIndicator}
+      />
+    </View>
+  );
 };
 
 export default SplashScreen;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -1,5 +1,16 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FAEBD7',
+  },
+  activityIndicator: {
+    alignItems: 'center',
+    height: 80,
+  },
+});
 
 export default styles;
